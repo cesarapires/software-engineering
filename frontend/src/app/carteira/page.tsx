fe85f86f4d4a2956cd9ddebdd46c98135ff1377d
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-03 00:04:33 -0300

    feat: integrate new wallet modal to wallet list page

--- frontend/src/app/carteira/page.tsx
+++ frontend/src/app/carteira/page.tsx
@@ -3,14 +3,17 @@
 import { DataTable } from '@/components/data-table/data-table'
 import { columns } from './columns'
 import Api from '@/lib/api'
-import { useEffect, useState } from 'react'
+import { Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react'
 import { ModalExcluirCarteira } from './modal-excluir-carteira'
 import { CarteiraListagem } from '@/types/carteiraListagem'
+import { Button } from '@/components/ui/button'
+import { CreateNewWalletModal } from './modal-nova-carteira'
 
 export default function Carteira() {
   const [data, setData] = useState<CarteiraListagem[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [open, setOpen] = useState<boolean>(false)
+  const [isExcluirModalOpen, setIsExcluirModalOpen] = useState<boolean>(false)
+  const [isCreateNewWalletModalOpen, setIsCreateNewWalletModalOpen] = useState<boolean>(false)
   const [selectedCarteira, setSelectedCarteira] = useState<CarteiraListagem>({
     id: 0,
     total: 0,
@@ -24,19 +27,41 @@ export default function Carteira() {
     })
   }, [isLoading])
 
+  const reloadData = () => {
+    setIsLoading(true)
+    Api.get<CarteiraListagem[]>('/v1/carteira/all').then(res => {
+      setData(res.data)
+      setIsLoading(false)
+    })
+  }
+
   return (
     <>
       <DataTable
         data={data}
-        columns={columns(setOpen, setSelectedCarteira)}
+        columns={columns(setIsExcluirModalOpen, setSelectedCarteira)}
         isLoading={isLoading}
+        optionButton={buildCreateNewWalletButton(setIsCreateNewWalletModalOpen)}
       />
       <ModalExcluirCarteira
-        open={open}
-        setOpen={setOpen}
+        open={isExcluirModalOpen}
+        setOpen={setIsExcluirModalOpen}
         carteira={selectedCarteira}
         setIsLoading={setIsLoading}
       />
+      <CreateNewWalletModal
+        open={isCreateNewWalletModalOpen}
+        setOpen={setIsCreateNewWalletModalOpen}
+        reloadData={reloadData}
+      />
     </>
+  )
+}
+
+function buildCreateNewWalletButton(setModalState: Dispatch<SetStateAction<boolean>>): ReactNode {
+  return (
+    <Button onClick={() => setModalState(true)}>
+      Criar nova carteira
+    </Button>
   )
 }
