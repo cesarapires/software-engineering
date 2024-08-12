2164dfa512afbb268e7026eec07eebab667ea544
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-12 20:37:01 -0300

    feat: add test e2e rf008

--- frontend/cypress/e2e/RF008.cy.ts
+++ frontend/cypress/e2e/RF008.cy.ts
@@ -22,17 +22,29 @@ describe('Login Page', () => {
   it('[RF008] Deve editar o nome da carteira', () => {
 	cy.contains('a', 'Carteiras').click()
 	
-	cy.get('[data-testid="data-table-row-actions__dropdown__edit-button"]').click();
+	cy.get('[data-testid="data-table-row-actions__dropdown-trigger__open-button"]').first().click()
 
+	cy.get('[data-testid="data-table-row-actions__dropdown__edit-button"]').click({ force: true })
 
-	createWallet('Minha nova carteira 2')
+    cy.get('input[name="description"]').type('Minha nova carteira editada')
+
+	cy.get('button[type="submit"]').click()
+
+	cy.contains('Minha nova carteira editada').should('be.visible')
   })
 
   it('[RF008E] Não deve editar o nome da carteira, quando se tenta renomear com um nome já existente', () => {
 	cy.contains('a', 'Carteiras').click()
 	
-	cy.contains('Minha nova carteira').should('be.visible')
+	cy.get('[data-testid="data-table-row-actions__dropdown-trigger__open-button"]').first().click()
 
-	createWallet('Minha nova carteira 2')
+	cy.get('[data-testid="data-table-row-actions__dropdown__edit-button"]').click({ force: true })
+
+	cy.get('input[name="description"]').type('Minha nova carteira editada')
+
+	cy.get('button[type="submit"]').click()
+
+	cy.contains('Erro ao editar carteira').should('be.visible')
+	cy.contains('Carteira já existe para esse usuário').should('be.visible')
   })
 })
