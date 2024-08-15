b1133ee63ed08271488c3c803b5ca75cf52ded43
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-15 08:59:14 -0300

    feat: add procedure to clean database on RF006

--- frontend/cypress/e2e/RF006.cy.ts
+++ frontend/cypress/e2e/RF006.cy.ts
@@ -2,34 +2,40 @@ import faker from '@faker-js/faker';
 import { makeLogin } from '../fixtures/makeLogin';
 import { User } from '../fixtures/types/user';
 import { createWallet } from '../fixtures/createWallet';
+import { createUser } from '../fixtures/createUser';
 
 describe('Login Page', () => {
 
 	let user: User
 
+	before(() => {
+		cy.visit('/')
+
+		user = {
+			name: 'Usuario Dummy',
+			email: 'usuario@dummy.com',
+			password: 'password'
+		}
+		createUser(user)
+	})
+
 	beforeEach(() => {
 	  cy.visit('/')
   
-	  user = {
-		  name: 'Usuario Dummy',
-		  email: 'usuario@dummy.com',
-		  password: 'password'
-	  }
-
 	  makeLogin(user)
 	})
 
-  it('[RF006] Deve adicionar nova carteira', () => {
-	cy.contains('a', 'Carteiras').click()
+	after(() => {
+		cy.resetDatabase()
+	})
 
+  it('[RF006] Deve adicionar nova carteira', () => {
 	createWallet('Minha nova carteira')
 
 	cy.contains('Minha nova carteira').should('be.visible')
   })
 
   it('[RF006E] Não eve adicionar nova carteira, quando a descrição já está em uso', () => {
-	cy.contains('a', 'Carteiras').click()
-
 	createWallet('Minha nova carteira')
 
 	cy.contains('Erro ao criar carteira').should('be.visible')
