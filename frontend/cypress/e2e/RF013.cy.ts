6647e8226d5c6d5f20271c8a5f6706f78d95534c
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-15 14:01:42 -0300

    feat: add procedure to clean database on RF013

--- frontend/cypress/e2e/RF013.cy.ts
+++ frontend/cypress/e2e/RF013.cy.ts
@@ -2,35 +2,45 @@ import faker from '@faker-js/faker';
 import { makeLogin } from '../fixtures/makeLogin';
 import { User } from '../fixtures/types/user';
 import { createWallet } from '../fixtures/createWallet';
+import { createUser } from '../fixtures/createUser';
 
 describe('Login Page', () => {
 
 	let user: User
 
-	beforeEach(() => {
-	  cy.visit('/')
-  
-	  user = {
-		  name: 'Usuario Dummy',
-		  email: 'usuario@dummy.com',
-		  password: 'password'
-	  }
+	before(() => {
+		cy.visit('/')
 
-	  makeLogin(user)
+		user = {
+			name: 'Usuario Dummy',
+			email: 'usuario@dummy.com',
+			password: 'password'
+		}
+		createUser(user)
+	})
+
+	beforeEach(() => {
+		cy.visit('/')
+
+		makeLogin(user)
+	})
+
+	after(() => {
+		cy.resetDatabase()
 	})
 
 	it('[RF013] Deve visualizar os detalhes da ação', () => {
 		cy.contains('a', 'Ações').click()
 		
-		cy.contains('NETEASE DRN').should('be.visible')
+		cy.contains('CIELO').should('be.visible')
 
 		cy.get('[data-testid="data-table-row-actions__dropdown-trigger__open-button"]').first().click()
 
 		cy.get('[data-testid="data-table-row-actions__dropdown__vizualize-button"]').click({ force: true })
 			
-		cy.contains('NetEase, Inc.').should('be.visible')
-		cy.contains('NETE34').should('be.visible')
-		cy.contains('Technology Services').should('be.visible')
+		cy.contains('CIELO').should('be.visible')
+		cy.contains('CIEL3L').should('be.visible')
+		cy.contains('Commercial Services').should('be.visible')
 		cy.contains('R$').should('be.visible')
 	  })
 })
