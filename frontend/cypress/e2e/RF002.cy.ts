6765ea7e8809c59037f3d19701c20e0ce429b2f1
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-15 08:47:22 -0300

    feat: add procedure to clean database on RF002

--- frontend/cypress/e2e/RF002.cy.ts
+++ frontend/cypress/e2e/RF002.cy.ts
@@ -1,29 +1,51 @@
+import { createUser } from "../fixtures/createUser"
 import { makeLogin } from "../fixtures/makeLogin"
 import { User } from "../fixtures/types/user"
 
 describe('Login Page', () => {
 
-  let user: User
+	let user: User
 
-  beforeEach(() => {
-	cy.visit('/')
+	before(() => {
+		cy.visit('/')
 
-	user = {
-		name: 'Usuario Dummy',
-		email: 'usuario@dummy.com',
-		password: 'password'
-	}
-  })
+		user = {
+			name: 'Usuario Dummy',
+			email: 'usuario@dummy.com',
+			password: 'password'
+		}
 
-  it('[RF002E] Não deve fazer o login quando as credenciais são inválidas', () => {
-	user.password = 'passwordd'
-	makeLogin(user)
-  })
+		createUser(user)	
+	})
 
-  it('RF002] Deve fazer o login quando as credenciais são válidas', () => {
-	makeLogin(user)
+	beforeEach(() => {
+		cy.visit('/')
 
-	cy.contains('Histórico de transações').should('be.visible')
-	cy.contains('Depositar').should('be.visible')
-  })
+		user = {
+			name: 'Usuario Dummy',
+			email: 'usuario@dummy.com',
+			password: 'password'
+		}
+	})
+
+	after(() => {
+		cy.resetDatabase()
+	})
+
+	it('RF002] Deve fazer o login quando as credenciais são válidas', () => {
+		makeLogin(user)
+
+		cy.contains('Histórico de transações').should('be.visible')
+		cy.contains('Depositar').should('be.visible')
+	})
+
+	it('[RF002E] Não deve fazer o login quando as credenciais são inválidas', () => {
+		user.password = 'passwordd'
+
+		makeLogin(user)
+
+		cy.contains('Email e/ou senha inválidos').should('be.visible')
+	})
+
+
 })
