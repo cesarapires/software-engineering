751fabe07c255db8b5187abeabb2ed0e6c3e630f
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-15 13:59:12 -0300

    feat: add procedure to clean database on RF010

---
+++ frontend/cypress/e2e/RF010.cy.ts
@@ -0,0 +1,49 @@
+import faker from '@faker-js/faker';
+import { makeLogin } from '../fixtures/makeLogin';
+import { User } from '../fixtures/types/user';
+import { createWallet } from '../fixtures/createWallet';
+import { createUser } from '../fixtures/createUser';
+import { buyStock } from '../fixtures/buyAction';
+import { addFunds } from '../fixtures/addFounds';
+
+describe('Login Page', () => {
+
+	let user: User
+
+	before(() => {
+		cy.visit('/')
+
+		user = {
+			name: 'Usuario Dummy',
+			email: 'usuario@dummy.com',
+			password: 'password'
+		}
+		createUser(user)
+	})
+
+	beforeEach(() => {
+		cy.visit('/')
+
+		makeLogin(user)
+	})
+
+	after(() => {
+		cy.resetDatabase()
+	})
+
+	it('[RF010] Deve visualizar a carteira', () => {
+		createWallet('Minha nova carteira')
+
+		addFunds(50000)
+
+		buyStock("BAIQ39", "1")
+
+		cy.contains('a', 'Carteiras').click()
+
+		cy.get('[data-testid="data-table-row-actions__dropdown-trigger__open-button"]').click()
+
+		cy.get('[data-testid="data-table-row-actions__dropdown__vizualize-button"]').click()
+
+		cy.contains('Minha nova carteira').should('be.visible')
+	})
+})
