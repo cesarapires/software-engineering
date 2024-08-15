ecf814fa7f9d22329c897ad20fe7d17a80ea478a
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-08-15 13:36:26 -0300

    feat: add procedure to clean database on RF012

--- frontend/cypress/e2e/RF012.cy.ts
+++ frontend/cypress/e2e/RF012.cy.ts
@@ -2,29 +2,38 @@ import faker from '@faker-js/faker';
 import { makeLogin } from '../fixtures/makeLogin';
 import { User } from '../fixtures/types/user';
 import { createWallet } from '../fixtures/createWallet';
+import { createUser } from '../fixtures/createUser';
 
 describe('Login Page', () => {
 
 	let user: User
 
-	beforeEach(() => {
-	  cy.visit('/')
-  
-	  user = {
-		  name: 'Usuario Dummy',
-		  email: 'usuario@dummy.com',
-		  password: 'password'
-	  }
+	before(() => {
+		cy.visit('/')
 
-	  makeLogin(user)
+		user = {
+			name: 'Usuario Dummy',
+			email: 'usuario@dummy.com',
+			password: 'password'
+		}
+		createUser(user)
+	})
+
+	beforeEach(() => {
+		cy.visit('/')
+
+		makeLogin(user)
+	})
+
+	after(() => {
+		cy.resetDatabase()
 	})
 
 	it('[RF012] Deve listar visualizar a listagem de carteiras', () => {
 		cy.contains('a', 'Ações').click()
-		
-		cy.contains('NETEASE DRN').should('be.visible')
-		cy.contains('UNITED RENTADRN').should('be.visible')
-		cy.contains('GX AI TECH DRE').should('be.visible')
-		cy.contains('ARES MANAGEMDRN').should('be.visible')
-	  })
+
+		cy.contains('CIELO').should('be.visible')
+		cy.contains('BKR SP100 DRE').should('be.visible')
+		cy.contains('ILLINOIS TOODRN').should('be.visible')
+	})
 })
